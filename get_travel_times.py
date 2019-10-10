cac61b262e9930969b76a6d5154593365bb77cdc
Christian Selig <cselig@uw.edu>
2019-10-10 00:05:43 -0700

    clean up code

--- get_travel_times.py
+++ get_travel_times.py
@@ -1,26 +1,44 @@
-import googlemaps
-from datetime import datetime
-from local import api_key
 import json
 import time
-import pandas as pd
+import datetime as dt
 from pprint import pprint
+from typing import List
+
+import googlemaps
+import pandas as pd
+
+from local import api_key
 
 
-HOME = '400 S Andresen Rd, Vancouver, WA 98661, USA'
-WORK = '2505 SE 11th Ave, Portland, OR 97202, USA'
+HOME = ''
+WORK = ''
+
 TRAFFIC_MODELS = ['optimistic', 'best_guess', 'pessimistic']
+TEST = True
+
+'''
+TODO:
+    - might be a way to reduce api calls?
+'''
 
 
-# params:
-#   gmaps: google maps client object
-#   start, end: addresses for home and work (strings)
-#   departure_time: datetime object
-#   traffic_model: one of 'optimistic', 'best_guess', 'pessimistic'
-# return 
-#   duration_in_traffic (int) in seconds
-# Note: will be called ~1000 times; Google maps API daily free request limit is 2500
-def get_single_data_point(gmaps, start, end, departure_time, traffic_model):
+def get_single_data_point(
+    gmaps: googlemaps.Client,
+    start: str,
+    end: str,
+    departure_time: dt.datetime,
+    traffic_model: str,
+) -> int:
+    '''
+    params:
+        gmaps: google maps client object
+        start, end: addresses for home and work (strings)
+        departure_time: datetime object
+        traffic_model: one of 'optimistic', 'best_guess', 'pessimistic'
+    return 
+        duration_in_traffic (int) in seconds
+    Note: will be called ~1000 times; Google maps API daily free request limit is 2500
+    '''
     result =  gmaps.distance_matrix(start, end, 
         mode='driving', departure_time=departure_time, traffic_model=traffic_model)
     # return preditced travel time in seconds
@@ -30,34 +48,50 @@ def get_single_data_point(gmaps, start, end, departure_time, traffic_model):
         return -1
 
 
-# params: 
-#   direction: either 'going' or 'returning'
-# returns:
-#   List of datetime objects: 6 hour range for every weekday starting at either 6am (direction='going')
-#   or 2pm (direction='returning'), every 10 minutes
-def generate_datetimes(direction):
+def generate_datetimes(direction: str) -> dt.datetime:
+    '''
+    params: 
+        direction: either 'going' or 'returning'
+    returns:
+        List of datetime objects: 6 hour range for every weekday starting at either 6am (direction='going')
+        or 2pm (direction='returning'), every 10 minutes
+    '''
     if direction == 'going':
         start_hour = 6
     elif direction == 'returning':
         start_hour = 14
 
-    return [datetime(2017, 10, 2 + day, start_hour + hour, 0 + minute, 0, 0) 
-                    for day in range(0, 5)
-                    for hour in range(0, 6)
-                    for minute in range(0, 60, 10)]
+    today = dt.datetime.now().replace(hour=start_hour, minute=0, second=0, microsecond=0)
+    tomorrow = today + dt.timedelta(1)
 
-    # smaller list for testing, only range of 6 hours during one day
-    # return [datetime(2017, 10, 2, start_hour + hour, 0, 0, 0) 
-    #             for hour in range(0, 6)]
+    if TEST:
+        # smaller time range for testing, only 6 hours over one day
+        return [
+            tomorrow + dt.timedelta(seconds=hour * 60 * 60)
+            for hour in range(0, 6)
+        ]
+    else:
+        return [
+            tomorrow + dt.timedelta(day=day, hour=hour, minute=minute)
+            for day in range(0, 5)
+            for hour in range(0, 6)
+            for minute in range(0, 60, 10)
+        ]
+        # return [datetime(2017, 10, 2 + day, start_hour + hour, 0 + minute, 0, 0) 
+        #                 for day in range(0, 5)
+        #                 for hour in range(0, 6)
+        #                 for minute in range(0, 60, 10)]
 
 
-# params:
-#   gmaps: google maps client object
-#   start, end: addresses for home and work (strings)
-#   datetimes: list of datetime objects
-# returns:
-#   DataFrame with traffic models for columns, datetimes for rows, and travel times for values
-def generate_df(gmaps, start, end, datetimes):
+def generate_df(gmaps: googlemaps.Client, start: str, end: str, datetimes: List[dt.datetime]):
+    '''
+    params:
+        gmaps: google maps client object
+        start, end: addresses for home and work (strings)
+        datetimes: list of datetime objects
+    returns:
+        DataFrame with traffic models for columns, datetimes for rows, and travel times for values
+    '''
     optimistic = [get_single_data_point(gmaps, start, end, time, 'optimistic') 
                     for time in datetimes]
     best_guess = [get_single_data_point(gmaps, start, end, time, 'best_guess') 
@@ -72,27 +106,33 @@ def generate_df(gmaps, start, end, datetimes):
                         columns=TRAFFIC_MODELS)
 
 
-# params:
-#   gmaps: google maps client object
-# Queries google maps api for morning commute, creates DataFrame and saves as pickle file
 def get_morning_data(gmaps):
+    '''
+    Queries google maps api for morning commute, creates DataFrame and saves as pickle file
+
+    params:
+        gmaps: google maps client object
+    '''
     start, end = HOME, WORK
     datetimes = generate_datetimes('going')
     going_df = generate_df(gmaps, start, end, datetimes)
-    going_df.to_pickle('going_df.p')
+    going_df.to_pickle('./data/going_df.p')
 
 
-# params:
-#   gmaps: google maps client object
-# Queries google maps api for afternoon commute, creates DataFrame and saves as pickle file
 def get_afternoon_data(gmaps):
+    '''
+    Queries google maps api for afternoon commute, creates DataFrame and saves as pickle file
+
+    params:
+        gmaps: google maps client object
+    '''
     start, end = WORK, HOME
     datetimes = generate_datetimes('returning')
     returning_df = generate_df(gmaps, start, end, datetimes)
-    returning_df.to_pickle('returning_df.p')
+    returning_df.to_pickle('./data/returning_df.p')
 
 
 if __name__ == '__main__':
-    gmaps = googlemaps.Client(key=api_key.KEY)
+    gmaps = googlemaps.Client(key=api_key)
     get_morning_data(gmaps)
     get_afternoon_data(gmaps)
